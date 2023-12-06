662e5f415aa358ffb4d19b5082302c5d3786b7e2
rafael.souza <rafael.souza@mgs.srv.br>
2023-12-06 16:06:02 -0300

    Dialog confirmacao deletar robo

--- src/components/pieces/RoboList.js
+++ src/components/pieces/RoboList.js
@@ -1,13 +1,50 @@
 import React, { Component } from "react";
-import { Button, ButtonGroup, Container, Table } from "react-bootstrap";
+import { Button, ButtonGroup, Container, Table, Modal } from "react-bootstrap";
 // import { Link } from 'react-router-dom';
 import { useNavigate } from "react-router-dom";
 import RoboService from "../services/RoboService";
 
+function MyVerticallyCenteredModal({ showModal, handleClose, modalData, deleteRobo }) {
+  return (
+    <Modal
+      show={showModal}
+      onHide={handleClose}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Deletar Robô
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h5>Tem certeza que deseja deletar o Robô com o ID {modalData}?</h5>
+        <p>Esta ação não poderá ser desfeita.</p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={handleClose}>Close</Button>
+        <Button
+          size="sm"
+          variant="danger"
+          onClick={deleteRobo}
+        >
+          Deletar
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
 class RoboList extends Component {
   constructor(props) {
     super(props);
-    this.state = { robos: [] };
+    this.state = {
+      robos: [],
+      modalShow: false,
+      inputId: "",
+      modalData: "",
+    };
     this.addRobo = this.addRobo.bind(this);
     this.editarRobo = this.editarRobo.bind(this);
     this.deletarRobo = this.deletarRobo.bind(this);
@@ -24,17 +61,36 @@ class RoboList extends Component {
     document.location.reload();
   }
 
-  deletarRobo(id) {
-    RoboService.deletarRobo(id).then(( res => {
-      this.setState({robos: this.state.robos.filter(robo => robo.id !== id)});
-    }))
+  editarRobo(id) {
+    this.props.history.push(`/editar/${id}`);
     document.location.reload();
   }
 
-  editarRobo(id){
-    this.props.history.push(`/editar/${id}`)
-    document.location.reload();
+  deletarRobo() {
+    RoboService.deletarRobo(this.state.inputId).then((res) => {
+      this.setState({
+        robos: this.state.robos.filter((robo) => robo.id !== this.state.inputId),
+      });
+    });
+    console.log("Robô id ", this.state.inputId, " deletado.");
+    //document.location.reload();
   }
+
+  closeModalHandle = () => {
+    this.setState({ modalShow: !this.state.modalShow });
+  };
+
+  openModalHandle = (id) => {
+    this.setState({ modalShow: !this.state.modalShow });
+    this.setState(
+      {
+        inputId: id,
+      },
+      () => {
+        console.log("Robô a ser deletado: id ", this.state.inputId);
+      }
+    );
+  };
 
   render() {
     //     const {robos, isLoading} = this.state;
@@ -60,10 +116,15 @@ class RoboList extends Component {
               >
                 Editar
               </Button>
-              <Button
+              {/* <Button
                 size="sm"
                 variant="danger"
                 onClick={() => this.deletarRobo(robo.id)}
+              > */}
+              <Button
+                size="sm"
+                variant="danger"
+                onClick={() => this.openModalHandle(robo.id)}
               >
                 Deletar
               </Button>
@@ -98,6 +159,12 @@ class RoboList extends Component {
             <tbody>{roboList}</tbody>
           </Table>
         </Container>
+        <MyVerticallyCenteredModal
+          showModal={this.state.modalShow}
+          handleClose={this.closeModalHandle}
+          modalData={this.state.inputId}
+          deleteRobo={this.deletarRobo}
+        />
       </div>
     );
   }
