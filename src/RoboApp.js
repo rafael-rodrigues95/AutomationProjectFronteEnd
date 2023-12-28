d7e4aa682fb8b69915e220aa34a64bf6ee1004cc
rafael.souza <rafael.souza@mgs.srv.br>
2023-12-28 16:36:10 -0300

    Ajustes Body.js

--- src/RoboApp.js
+++ src/RoboApp.js
@@ -2,10 +2,6 @@ import React, { Component } from "react";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
-import RoboList from "./components/pieces/RoboList";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import RoboCriar from "./components/pieces/RoboCriar";
-import RoboEditar from "./components/pieces/RoboEditar";
 // Bootstrap CSS
 import "bootstrap/dist/css/bootstrap.min.css";
 // Bootstrap Bundle JS
@@ -24,18 +20,8 @@ function BasicLayout() {
 class RoboApp extends Component {
   render() {
     return (
-      <div className="container">
-        
-        <Router>
-          <div className="container">
-            <BasicLayout/>
-            <Switch>
-              <Route exact path="/" component={RoboList}></Route>
-              <Route path="/criar" component={RoboCriar}></Route>
-              <Route path="/editar/:id" component={RoboEditar}></Route>
-            </Switch>
-          </div>
-        </Router>
+      <div>
+      <BasicLayout/>
       </div>
     );
   }
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,24 @@
 import React, { Component } from "react";
+import RoboList from "./pieces/RoboList";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import RoboCriar from "./pieces/RoboCriar";
+import RoboEditar from "./pieces/RoboEditar";
 
 
 class Body extends Component {
   render() {
     return (
-      <div></div>
+      <div>
+                <Router>
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={RoboList}></Route>
+              <Route path="/criar" component={RoboCriar}></Route>
+              <Route path="/editar/:id" component={RoboEditar}></Route>
+            </Switch>
+          </div>
+        </Router>
+      </div>
     )
   }
 }
